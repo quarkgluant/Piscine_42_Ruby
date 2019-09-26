98126a0d59ae59bd7eec2b06ea21e7fbd54dc31c
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2019-09-26 16:47:10 +0200

    Update schema.rb

--- day_06/ex00/LifeProTips/db/schema.rb
+++ day_06/ex00/LifeProTips/db/schema.rb
@@ -11,14 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170412182608) do
+ActiveRecord::Schema.define(version: 20190925151928) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.string   "password"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
