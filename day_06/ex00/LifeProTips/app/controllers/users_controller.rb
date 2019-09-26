fda0e91b21faec7e80902de6a5cdd907ab3a95d8
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2019-09-26 16:29:48 +0200

    update users_controller.rb

--- day_06/ex00/LifeProTips/app/controllers/users_controller.rb
+++ day_06/ex00/LifeProTips/app/controllers/users_controller.rb
@@ -1,40 +1,33 @@
 class UsersController < ApplicationController
 
-  def index
-    
-  end
+  def index; end
 
   def new
     @user = User.new
   end
 
   def create
-    if params[:user][:password] == params[:user][:password]
-      @user = User.new(user_params)
-      @user.save
+    @user = User.new(user_params)
+    @user.email.downcase!
+    if @user.save
+      # If user saves in the db successfully:
+      flash[:notice] = 'Compte créé avec succès'
+      session[:user_id] = @user.id.to_s
+      redirect_to root_path, notice: 'vous êtes connecté !'
+    else
+      # If user fails model validation - probably a bad password or duplicate email:
+      flash.now.alert = 'Oups, création impossible...'
+      redirect_to root_path
     end
-
   end
 
-  def login
+  def sign_in
+    @user = User.new
   end
-
-  def logout
-  end
-
-  def connect
-  end
-
-  def signin
-  end
-
 
   private
-    def user_params
-      params.require(:user).permit(:name, :email, :password)
-    end
 
-    def user_connect_params
-      params.require(:user).permit(:email, :password)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
