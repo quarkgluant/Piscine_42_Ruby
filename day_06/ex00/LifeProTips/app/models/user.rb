f69ed5a09b22c158387a8dd6896a87c58c4d75e9
Patrice Cadiot <quarkgluant@users.noreply.github.com>
2019-09-26 16:34:47 +0200

    Update user.rb

--- day_06/ex00/LifeProTips/app/models/user.rb
+++ day_06/ex00/LifeProTips/app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-  validates_uniqueness_of :name
+  has_secure_password
+  validates :name, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }
 end
+
